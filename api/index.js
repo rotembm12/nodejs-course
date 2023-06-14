4845bccb402fe8949fb6f418b6cb02f60a1ec207
rotembm12 <rotembm12@gmail.com>
2023-06-14 13:18:28 +0200

    api is where the server routers gets called

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ Hello: "world" });
 });
 // app.get("/", (req, res) => {
